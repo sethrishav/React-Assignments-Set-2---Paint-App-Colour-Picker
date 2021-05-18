d98bf9079578ee73391b9b42ae9362d0a967931e
Rishav Sharma <61363076+sethrishav@users.noreply.github.com>
2021-05-18 20:24:27 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 
 const Selection=(props)=>{
-  const [var, func]=useState({background:''})
+  const {applyColor}=props
+  const [var, func]=useState({background:''});
   return(
   <div onClick={applyColor(func)} style={var} >
     <h2 class="subheading">Selection</h2>
     </div>
-  )
+  );
 }
 export default Selection;
